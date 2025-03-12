e1d4e0b59145540fae92debdd311831f7f5c8473
jhssong <jhssong02@gmail.com>
2025-03-13 00:11:53 +0900

    init

---
+++ src/main/java/com/gdgknu/Inveed/InveedApplication.java
@@ -0,0 +1,13 @@
+package com.gdgknu.Inveed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InveedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InveedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdgknu/Inveed/InveedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdgknu.Inveed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InveedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
