76e468814e8e617035fd7167fd580c3e02797e68
junhyung85920 <junhyung85920@gmail.com>
2025-03-27 01:00:29 +0900

    chore: Apply webconfig

---
+++ src/main/java/com/gdgknu/Inveed/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.gdgknu.Inveed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // TODO Add security with CORS before production
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
