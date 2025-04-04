9f764886714f61e6cc0bc5485c6d7f7c742e4c7c
jhssong <jhssong02@gmail.com>
2025-04-04 22:34:59 +0900

    chore: Add api descriptions for swagger

--- src/main/java/com/gdgknu/Inveed/domain/StockRank/controller/StockRankController.java
+++ src/main/java/com/gdgknu/Inveed/domain/StockRank/controller/StockRankController.java
@@ -6,6 +6,7 @@ import com.gdgknu.Inveed.domain.StockRank.service.StockRankService;
 import com.gdgknu.Inveed.response.ResponseUtil;
 import com.gdgknu.Inveed.response.SuccessCode;
 import com.gdgknu.Inveed.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -25,24 +26,28 @@ public class StockRankController {
     private final StockRankService stockRankService;
 
     @GetMapping("/trade-vol")
+    @Operation(summary = "Overseas Stock Ranking by Trading Volume")
     public ResponseEntity<SuccessResponse<List<StockRankResDTO>>> getTradeVol(@RequestBody StockRankReqDTO stockRankReqDTO) {
         List<StockRankResDTO> stockRankResDTO = stockRankService.getTradeVol(stockRankReqDTO);
         return ResponseUtil.buildSuccessResponse(SuccessCode.STOCK_RANK_VOL_SUCCESS, stockRankResDTO);
     }
 
     @GetMapping("/trade-pbmn")
+    @Operation(summary = "Overseas Stock Ranking by Trading Value")
     public ResponseEntity<SuccessResponse<List<StockRankResDTO>>> getTradePbmn(@RequestBody StockRankReqDTO stockRankReqDTO) {
         List<StockRankResDTO> stockRankResDTO = stockRankService.getTradePbmn(stockRankReqDTO);
         return ResponseUtil.buildSuccessResponse(SuccessCode.STOCK_RANK_PBMN_SUCCESS, stockRankResDTO);
     }
 
     @GetMapping("/trade-growth")
+    @Operation(summary = "Overseas Stock Ranking by Trading Volume Growth Rate")
     public ResponseEntity<SuccessResponse<List<StockRankResDTO>>> getTradeGrowth(@RequestBody StockRankReqDTO stockRankReqDTO) {
         List<StockRankResDTO> stockRankResDTO = stockRankService.getTradeGrowth(stockRankReqDTO);
         return ResponseUtil.buildSuccessResponse(SuccessCode.STOCK_RANK_GROWTH_SUCCESS, stockRankResDTO);
     }
 
     @GetMapping("/trade-turnover")
+    @Operation(summary = "Overseas Stock Ranking by Turnover Rate (Popularity)")
     public ResponseEntity<SuccessResponse<List<StockRankResDTO>>> getTradeTurnover(@RequestBody StockRankReqDTO stockRankReqDTO) {
         List<StockRankResDTO> stockRankResDTO = stockRankService.getTradeTurnover(stockRankReqDTO);
         return ResponseUtil.buildSuccessResponse(SuccessCode.STOCK_RANK_TRUNOVER_SUCCESS, stockRankResDTO);
