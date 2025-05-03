d4dd3ccb67553ff6727eb43858c1afbeddcb168b
jhssong <jhssong02@gmail.com>
2025-05-03 12:34:06 +0900

    fix: Fix login session authentication issue

--- src/main/java/com/gdgknu/Inveed/config/SecurityConfig.java
+++ src/main/java/com/gdgknu/Inveed/config/SecurityConfig.java
@@ -39,8 +39,6 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                // TODO Remove session
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
