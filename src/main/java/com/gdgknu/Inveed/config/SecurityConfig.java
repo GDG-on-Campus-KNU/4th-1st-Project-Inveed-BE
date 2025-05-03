6e8c8f5341242b68f461096f3b425375213183b5
jhssong <jhssong02@gmail.com>
2025-05-03 21:30:00 +0900

    feat: Update response entity in AuthController

--- src/main/java/com/gdgknu/Inveed/config/SecurityConfig.java
+++ src/main/java/com/gdgknu/Inveed/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/oauth2/authorization/google").permitAll()
+                        .requestMatchers("/api/auth/kinvest-login").authenticated()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/v1/**").authenticated()
 //                        .anyRequest().denyAll()
