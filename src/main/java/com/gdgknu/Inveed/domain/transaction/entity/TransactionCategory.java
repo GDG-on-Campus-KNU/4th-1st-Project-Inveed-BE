2ca93317e1e3ef8f85ae0672aebf08132e3cdb60
chominju <cmj021002@knu.ac.kr>
2025-05-09 17:15:14 +0900

    feat: add TransactionCategory, TransactionType

---
+++ src/main/java/com/gdgknu/Inveed/domain/transaction/entity/TransactionCategory.java
@@ -0,0 +1,13 @@
+package com.gdgknu.Inveed.domain.transaction.entity;
+
+public enum TransactionCategory {
+    FOOD,              // 식비
+    CAFE,        // 카페/간식
+    ENTERTAINMENT,     // 술/유흥
+    LIVING,            // 생활
+    BEAUTY,            // 뷰티/미용
+    FASHION,           // 패션
+    HOUSING,      // 주거/통신
+    MEDICAL,           // 의료/건강
+    ETC                // 기타
+}
